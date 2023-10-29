ce5083b40540851b59abf67e5da46061a13240dc
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-10-30 02:58:30 +0900

    include script

--- src/scripts/get_artifact_url.sh
+++ src/scripts/get_artifact_url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-project_slug=$(echo "$CIRCLE_BUILD_URL" | sed -e "s|https://circleci.com/%7C%7Cg" -e "s|/[0-9]*$||g")
+project_slug=$(echo "$CIRCLE_BUILD_URL" | sed -e "s|https://circleci.com/||g" -e "s|/[0-9]*$||g")
 artifacts=$(curl -X GET "https://circleci.com/api/v2/project/$project_slug/$CIRCLE_BUILD_NUM/artifacts" \
 -H "Accept: application/json" \
 -u "$CIRCLE_API_TOKEN:")
