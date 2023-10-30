54a67cfb0ebe65f01c9a587e3492a6fe8bec6d39
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-10-30 17:38:37 +0900

    Bulma framework

--- src/scripts/generate_page.sh
+++ src/scripts/generate_page.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 cat > page.html <<- _EOF_
+<!DOCTYPE html>
 <html>
   <head>
-    <title>My System Information</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
   </head>
-
   <body>
-    <h1>My System Information</h1>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
   </body>
 </html>
 _EOF_
