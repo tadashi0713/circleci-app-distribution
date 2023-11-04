f7f666e0d4b9df62d463d93ba7f5e362999e49a2
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-11-05 06:06:47 +0900

    apt update

--- src/scripts/install_qrencode.sh
+++ src/scripts/install_qrencode.sh
@@ -3,7 +3,7 @@
 if [ "$(uname)" == 'Darwin' ]; then
   brew install qrencode
 elif [ "$(expr substr "$(uname -s)" 1 5)" == 'Linux' ]; then
-  sudo apt-get install qrencode
+  sudo apt update && sudo apt install qrencode
 else
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
