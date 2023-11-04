01f5d2c3fcd7d771173c7300ce69c62e81e519f8
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-11-05 05:58:04 +0900

    exclude

--- src/scripts/install_qrencode.sh
+++ src/scripts/install_qrencode.sh
@@ -2,7 +2,7 @@
 
 if [ "$(uname)" == 'Darwin' ]; then
   brew install qrencode
-elif [ "${"$(uname -s)":1:5}" == 'Linux' ]; then
+elif [ "$(expr substr "$(uname -s)" 1 5)" == 'Linux' ]; then
   sudo apt-get install qrencode
 else
   echo "Your platform ($(uname -a)) is not supported."
