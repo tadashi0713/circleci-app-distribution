7b9661223f5b05b72b6c81f403d87bf256b44d12
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-12-08 12:52:16 +1100

    iOS not to use api

--- src/scripts/get_latest_artifact_url.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-project_slug=$(echo "$CIRCLE_BUILD_URL" | sed -e "s|https://circleci.com/||g" -e "s|/[0-9]*$||g")
-artifacts=$(curl -X GET "https://circleci.com/api/v2/project/$project_slug/$CIRCLE_BUILD_NUM/artifacts" \
--H "Accept: application/json" \
--u "$CIRCLE_API_TOKEN:")
-echo "export $ARTIFACT_URL_NAME=$(echo "$artifacts" | jq '.items[0].url')" >> "$BASH_ENV"
--- src/scripts/show_qr_cli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-printf 'You can now install and debug app via CircleCI App Distribution!\n\n%s\n\n\n' "$PAGE_URL"
-qrencode -t ansiutf8 "$PAGE_URL"
