f99aae1c01a3c69127cb4d04e5d0c5daf7d0dd92
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2023-12-26 14:16:12 +0900

    generate_slack_template

---
+++ src/scripts/generate_slack_template.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+template=$(cat << EOS
+{
+  "blocks": [
+    {
+      "type": "section",
+      "fields": [
+        {
+          type: "mrkdwn",
+          text: "*Build URL:* <${CIRCLE_BUILD_URL}|${CIRCLE_BUILD_NUM}>"
+        },
+        {
+          type: "mrkdwn",
+          text: "*Branch:* ${CIRCLE_BRANCH}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*Name:* ${APP_NAME}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*Identifier:* ${IDENTIFIER}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*ReleaseVersion:* ${RELEASE_VERSION}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*BuildVersion:* ${BUILD_VERSION}"
+        },
+        {
+          type: "mrkdwn",
+          text: "*Release Note:* ${RELEASE_NOTE}"
+        }
+      ]
+    }
+  ]
+}
+EOS
+)
+
+echo "$template" | jq > /tmp/AppDistributionSlackTemplate.json
+echo 'export APP_DISTRIBUTION_SLACK_TEMPLATE=$(cat /tmp/AppDistributionSlackTemplate.json)' >> "$BASH_ENV"
