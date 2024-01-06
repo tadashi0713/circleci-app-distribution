f43d88bfb60039ed75dd5fd46e90d4838c85e378
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2024-01-06 16:35:32 +0900

    gh pr list

--- src/scripts/comment_github_pr.sh
+++ src/scripts/comment_github_pr.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-pr_response=$(gh api \
--H "Accept: application/vnd.github+json" \
--H "X-GitHub-Api-Version: 2022-11-28" \
--H "Authorization: Bearer $GITHUB_TOKEN" \
-/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open
-)
+pr_number=$(gh pr list --head github_pr --state open --json number --jq ".[].number")
 
-if [ $(echo $pr_response | jq length) -eq 0 ]; then
+if [ $pr_number -eq "" ]; then
   echo "No PR found to update"
 else
-  pr_number=$(echo $pr_response | jq -r ".[].number")
+  gh pr comment $pr_number --body "
+  # CircleCI App Distribution
+  "
 fi
-
-echo $pr_number
-
-gh pr comment $pr_number --body "
-# CircleCI App Distribution
-"
