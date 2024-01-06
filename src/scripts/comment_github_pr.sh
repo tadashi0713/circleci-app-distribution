f183339a0f2ca8c4e2b58234b00a628e1b683bc4
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2024-01-06 16:15:38 +0900

    Comment

--- src/scripts/comment_github_pr.sh
+++ src/scripts/comment_github_pr.sh
@@ -7,23 +7,12 @@ pr_response=$(gh api \
 /repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open
 )
 
-# $(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" \
-# -H "Accept: application/vnd.github+json" \
-# -H "Authorization: Bearer $GITHUB_TOKEN" \
-# -H "X-GitHub-Api-Version: 2022-11-28" \
-# )
+if [ $(echo $pr_response | jq length) -eq 0 ]; then
+  echo "No PR found to update"
+else
+  pr_number=$(echo $pr_response | jq -r ".[]._links.number")
+fi
 
-echo $pr_response
-
-# if [ $(echo $pr_response | jq length) -eq 0 ]; then
-#   echo "No PR found to update"
-# else
-#   pr_comment_url=$(echo $pr_response | jq -r ".[]._links.comments.href")
-# fi
-
-# curl --location --request POST "$pr_comment_url" \
-# -u $GH_USER:$GH_TOKEN \
-# --header 'Content-Type: application/json' \
-# --data-raw '{
-# "body": "This would be the data to add"
-# }'
+gh pr comment $pr_number --body "
+# CircleCI App Distribution
+"
