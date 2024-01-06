011a195ed025186559b27e75f8d795feefe9806c
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2024-01-06 16:43:23 +0900

    Add info

--- src/scripts/comment_github_pr.sh
+++ src/scripts/comment_github_pr.sh
@@ -7,5 +7,11 @@ if [ $pr_number -eq "" ]; then
 else
   gh pr comment $pr_number --body "
   # CircleCI App Distribution
+  | Name | ${APP_NAME} |
+  | ReleaseVersion | ${RELEASE_VERSION} |
+  | BuildVersion | ${BUILD_VERSION} |
+  | Identifier | ${IDENTIFIER} |
+  | Release Note | ${RELEASE_NOTE} |
+  | Install URL | ${PAGE_URL} |
   "
 fi
