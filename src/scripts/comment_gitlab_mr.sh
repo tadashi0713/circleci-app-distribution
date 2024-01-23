e92c16f73943811b29a1aef7d9b4fd20ee210421
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2024-01-23 14:00:32 +0900

    Comment to GitHub MR

---
+++ src/scripts/comment_gitlab_mr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install Glab
+if [ "$(uname)" == 'Darwin' ]; then
+  brew install glab
+elif [ "$(expr substr "$(uname -s)" 1 5)" == 'Linux' ]; then
+  curl -sSL "https://raw.githubusercontent.com/upciti/wakemeops/main/assets/install_repository" | sudo bash
+  sudo apt install glab
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
+
+# Comment to Merge Request
+glab mr note --message "
+# CircleCI App Distribution
+|   |   |
+|---|---|
+| Platform | ${PLATFORM} |
+| Name | ${APP_NAME} |
+| ReleaseVersion | ${RELEASE_VERSION} |
+| BuildVersion | ${BUILD_VERSION} |
+| Identifier | ${IDENTIFIER} |
+| Install URL | ${PAGE_URL} |
+| QR Code | ![](${QR_URL}) |
+"
